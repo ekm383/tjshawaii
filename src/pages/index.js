44d98785982ed41a19babcaac1ac5f66dcf5ec2c
Eric Manongdo <eric@superscriptcreative.com>
2020-04-28 15:08:49 -1000

    Menu Live

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
-import React from "react"
+import React, { Component } from "react"
 import { Helmet } from "react-helmet"
 import Layout from "../components/layout"
-import AniLink from "gatsby-plugin-transition-link/AniLink"
 import SEO from "../components/seo"
 import HeaderIndex from "../components/globals/header/HeaderIndex"
 import Banner from "../components/globals/banner/Banner"
@@ -12,186 +11,188 @@ import Menu from "../components/Menu"
 import Intro from "../components/Intro"
 import Gallery from "../components/Gallery"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO
-      title="Home"
-      description="TJ's Sports Bar is located on the corner of Kapiolani and South Street in Honolulu, Hawaii. Upbeat sports bar with billiard tables, darts, and jukebox, with an eclectic pub grub menu.`,
+export default class IndexPage extends Component {
+  render() {
+    return (
+      <Layout>
+        <SEO
+          title="Home"
+          description="TJ's Sports Bar is located on the corner of Kapiolani and South Street in Honolulu, Hawaii. Upbeat sports bar with billiard tables, darts, and jukebox, with an eclectic pub grub menu.`,
       author: `@restaurantmarketinghawaii"
-      keywords={[
-        `Honolulu Bar`,
-        `Hawaii Restaurant`,
-        `Hawaii Bar Food`,
-        `Honolulu Takeout`,
-        `Honolulu Pub Food`,
-        `Honolulu Bar Food`,
-      ]}
-    />
-    <HeaderIndex>
-      <Section style={{ width: "100vw", alignItems: "center" }}>
-        <Banner subtitle="TJ's Sports Bar is located on the corner of Kapiolani and South Street in Honolulu, Hawaii. We're an upbeat sports bar with  billiard tables, darts, a jukebox, and eclectic pub grub menu.">
-          <AniLink>
-            <Button className="upserve-olo-opener">ORDER TAKEOUT</Button>
-          </AniLink>
-        </Banner>
-      </Section>
-    </HeaderIndex>
-    <Gallery />
-    <Section style={{ marginTop: "4rem", marginBottom: "1rem" }}>
-      <Intro
-        heading="NEW LUNCH MENU"
-        subheading="We are open for takeout or delivery. Please call (808) 545-2424. Free delivery is available: Puncbowl to Pensacola and Beretania to Ala Moana."
-      />
-    </Section>
-    <Section id="menu">
-      <Menu
-        itemName="Tofu Salad"
-        itemDescription="watercress, pulled salmon, red onions, tomato, tofu, with house dressing"
-        price="8.00"
-      />
-      <Menu
-        itemName="Fried Saimin"
-        itemDescription="yakisoba, spam, kamaboko"
-        price="8.00"
-      />
-      <Menu
-        itemName="Fried Rice"
-        itemDescription="bacon, onion, carrot, eggs. (Garlic $7, Kim Chee $8, Lup Chong $9, Shrimp $10)"
-        price="6.00"
-      />
-      <Menu
-        itemName="Ahi Poke Bowl"
-        itemDescription="fresh ahi, white rice, furikake, onion, limu, green onion, soy sauce, sesame seeds"
-        price="10.00"
-      />
-      <Menu
-        itemName="Spicy Ahi Bowl"
-        itemDescription="fresh ahi, white rice, furikake, spicy aioli, green onion, nori sheets"
-        price="10.00"
-      />
-      <Menu
-        itemName="Cheeseburger"
-        itemDescription="all beef patty, cheddar cheese, onion, lettuce, tomato, burger spread, bun, crinkle fries"
-        price="10.00"
-      />
-      <Menu
-        itemName="BLT"
-        itemDescription="white bread, bacon, lettuce, tomato, crinkle fries"
-        price="6.00"
-      />
-      <Menu
-        itemName="Spam Egg"
-        itemDescription="white bead, crinkle fries"
-        price="6.00"
-      />
-      <Menu
-        itemName="Tuna Melt"
-        itemDescription="tuna, mayo, cheddar cheese, crinkle fries"
-        price="6.00"
-      />
-      <Menu
-        itemName="Miso Ramen"
-        itemDescription="fried shrimp, fish cake, green onions"
-        price="8.00"
-      />
-    </Section>
-    <Section style={{ marginTop: "4rem", marginBottom: "1rem" }}>
-      <Intro
-        heading="plate lunch menu"
-        subheading="Served with white rice and salad. We are open for takeout or delivery. Please call (808) 545-2424. Free delivery is available: Puncbowl to Pensacola and Beretania to Ala Moana."
-      />
-    </Section>
-    <Section id="menu">
-      <Menu
-        itemName="new york steak"
-        itemDescription="grilled, sauteed mushrooms and onions, choice of temperature"
-        price="10.00"
-      />
-      <Menu
-        itemName="chopped steak"
-        itemDescription="chuck steak, onion, carrot, celery, bell peppers, sweet soy, oyster sauce"
-        price="8.00"
-      />
-      <Menu
-        itemName="pork chops"
-        itemDescription="lightly battered, deep fried"
-        price="10.00"
-      />
-      <Menu
-        itemName="spicy pork"
-        itemDescription="smoked pork, sauteed onion, chili pepper"
-        price="10.00"
-      />
-      <Menu
-        itemName="kalbi"
-        itemDescription="bone-in, house marinades"
-        price="10.00"
-      />
-      <Menu
-        itemName="korean chicken"
-        itemDescription="lightly battered, sweet soy, sesame, chili"
-        price="8.00"
-      />
-      <Menu
-        itemName="garlic chicken"
-        itemDescription="lightly battered, sweet soy, garlic, furikake"
-        price="8.00"
-      />
-      <Menu
-        itemName="fried chicken"
-        itemDescription="chopped, boneless, skin-on"
-        price="8.00"
-      />
-      <Menu
-        itemName="wing zings"
-        itemDescription="mild spicy chicken wings, deep fried, ranch"
-        price="8.00"
-      />
-      <Menu
-        itemName="furikake ahi"
-        itemDescription="panko furikake crusted, wasabe aioli"
-        price="10.00"
-      />
-      <Menu
-        itemName="blackened ahi"
-        itemDescription="rolled on blackened seasoning, sashimi style, sweet soy dijon sauce"
-        price="10.00"
-      />
-      <Menu
-        itemName="ahi belly"
-        itemDescription="deep fried, lightly battered, ponzu"
-        price="10.00"
-      />
-      <Menu
-        itemName="steamed fish"
-        itemDescription="basa, sesame, ginger, garlic, cilantro, soy"
-        price="8.00"
-      />
-      <Menu
-        itemName="Salt Pepper Shrimp"
-        itemDescription="peeled, garlic, green onions"
-        price="10.00"
-      />
-      <Menu
-        itemName="calamari"
-        itemDescription="buttermilk, battered"
-        price="10.00"
-      />
-    </Section>
-    <Section style={{ marginTop: "4rem", marginBottom: "1rem" }}>
-      <Intro heading="contact us" />
-    </Section>
-    <Section id="information">
-      <ContactForm />
-    </Section>
-    <Helmet>
-      <script
-        src="https://app.upserve.com/platform/olo-widget.js?tjs-bar-and-grill-honolulu"
-        type="text/javascript"
-        id="upserve-olo-script"
-      ></script>
-    </Helmet>
-  </Layout>
-)
-
-export default IndexPage
+          keywords={[
+            `Honolulu Bar`,
+            `Hawaii Restaurant`,
+            `Hawaii Bar Food`,
+            `Honolulu Takeout`,
+            `Honolulu Pub Food`,
+            `Honolulu Bar Food`,
+          ]}
+        />
+        <HeaderIndex>
+          <Section style={{ width: "100vw", alignItems: "center" }}>
+            <Banner subtitle="TJ's Sports Bar is located on the corner of Kapiolani and South Street in Honolulu, Hawaii. We're an upbeat sports bar with  billiard tables, darts, a jukebox, and eclectic pub grub menu.">
+              <a href="https://app.upserve.com/s/tjs-bar-and-grill-honolulu">
+                <Button className="upserve-olo-opener">ORDER TAKEOUT</Button>
+              </a>
+            </Banner>
+          </Section>
+        </HeaderIndex>
+        <Gallery />
+        <Section style={{ marginTop: "4rem", marginBottom: "1rem" }}>
+          <Intro
+            heading="NEW LUNCH MENU"
+            subheading="We are open for takeout or delivery. Please call (808) 545-2424. Free delivery is available: Puncbowl to Pensacola and Beretania to Ala Moana."
+          />
+        </Section>
+        <Section id="menu">
+          <Menu
+            itemName="Tofu Salad"
+            itemDescription="watercress, pulled salmon, red onions, tomato, tofu, with house dressing"
+            price="8.00"
+          />
+          <Menu
+            itemName="Fried Saimin"
+            itemDescription="yakisoba, spam, kamaboko"
+            price="8.00"
+          />
+          <Menu
+            itemName="Fried Rice"
+            itemDescription="bacon, onion, carrot, eggs. (Garlic $7, Kim Chee $8, Lup Chong $9, Shrimp $10)"
+            price="6.00"
+          />
+          <Menu
+            itemName="Ahi Poke Bowl"
+            itemDescription="fresh ahi, white rice, furikake, onion, limu, green onion, soy sauce, sesame seeds"
+            price="10.00"
+          />
+          <Menu
+            itemName="Spicy Ahi Bowl"
+            itemDescription="fresh ahi, white rice, furikake, spicy aioli, green onion, nori sheets"
+            price="10.00"
+          />
+          <Menu
+            itemName="Cheeseburger"
+            itemDescription="all beef patty, cheddar cheese, onion, lettuce, tomato, burger spread, bun, crinkle fries"
+            price="10.00"
+          />
+          <Menu
+            itemName="BLT"
+            itemDescription="white bread, bacon, lettuce, tomato, crinkle fries"
+            price="6.00"
+          />
+          <Menu
+            itemName="Spam Egg"
+            itemDescription="white bead, crinkle fries"
+            price="6.00"
+          />
+          <Menu
+            itemName="Tuna Melt"
+            itemDescription="tuna, mayo, cheddar cheese, crinkle fries"
+            price="6.00"
+          />
+          <Menu
+            itemName="Miso Ramen"
+            itemDescription="fried shrimp, fish cake, green onions"
+            price="8.00"
+          />
+        </Section>
+        <Section style={{ marginTop: "4rem", marginBottom: "1rem" }}>
+          <Intro
+            heading="plate lunch menu"
+            subheading="Served with white rice and salad. We are open for takeout or delivery. Please call (808) 545-2424. Free delivery is available: Puncbowl to Pensacola and Beretania to Ala Moana."
+          />
+        </Section>
+        <Section id="menu">
+          <Menu
+            itemName="new york steak"
+            itemDescription="grilled, sauteed mushrooms and onions, choice of temperature"
+            price="10.00"
+          />
+          <Menu
+            itemName="chopped steak"
+            itemDescription="chuck steak, onion, carrot, celery, bell peppers, sweet soy, oyster sauce"
+            price="8.00"
+          />
+          <Menu
+            itemName="pork chops"
+            itemDescription="lightly battered, deep fried"
+            price="10.00"
+          />
+          <Menu
+            itemName="spicy pork"
+            itemDescription="smoked pork, sauteed onion, chili pepper"
+            price="10.00"
+          />
+          <Menu
+            itemName="kalbi"
+            itemDescription="bone-in, house marinades"
+            price="10.00"
+          />
+          <Menu
+            itemName="korean chicken"
+            itemDescription="lightly battered, sweet soy, sesame, chili"
+            price="8.00"
+          />
+          <Menu
+            itemName="garlic chicken"
+            itemDescription="lightly battered, sweet soy, garlic, furikake"
+            price="8.00"
+          />
+          <Menu
+            itemName="fried chicken"
+            itemDescription="chopped, boneless, skin-on"
+            price="8.00"
+          />
+          <Menu
+            itemName="wing zings"
+            itemDescription="mild spicy chicken wings, deep fried, ranch"
+            price="8.00"
+          />
+          <Menu
+            itemName="furikake ahi"
+            itemDescription="panko furikake crusted, wasabe aioli"
+            price="10.00"
+          />
+          <Menu
+            itemName="blackened ahi"
+            itemDescription="rolled on blackened seasoning, sashimi style, sweet soy dijon sauce"
+            price="10.00"
+          />
+          <Menu
+            itemName="ahi belly"
+            itemDescription="deep fried, lightly battered, ponzu"
+            price="10.00"
+          />
+          <Menu
+            itemName="steamed fish"
+            itemDescription="basa, sesame, ginger, garlic, cilantro, soy"
+            price="8.00"
+          />
+          <Menu
+            itemName="Salt Pepper Shrimp"
+            itemDescription="peeled, garlic, green onions"
+            price="10.00"
+          />
+          <Menu
+            itemName="calamari"
+            itemDescription="buttermilk, battered"
+            price="10.00"
+          />
+        </Section>
+        <Section style={{ marginTop: "4rem", marginBottom: "1rem" }}>
+          <Intro heading="contact us" />
+        </Section>
+        <Section id="information">
+          <ContactForm />
+        </Section>
+        <Helmet>
+          <script
+            src="https://app.upserve.com/platform/olo-widget.js?tjs-bar-and-grill-honolulu"
+            type="text/javascript"
+            id="upserve-olo-script"
+          ></script>
+        </Helmet>
+      </Layout>
+    )
+  }
+}
